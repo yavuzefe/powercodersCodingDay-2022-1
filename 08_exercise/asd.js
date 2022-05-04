6073ff05a9d57d2c5394034628ca0e70bf4a9482
yavuzefe <yavuzefe@gmail.com>
2022-05-04 09:19:14 +0200

    delete asd.js

--- 08_exercise/asd.js
+++
@@ -1,6 +0,0 @@
-let questionsList = [1, 2, 3, 4, 5];
-function randomizeArray(array){
-    array.sort(()=> Math.random() - 0.5);
-}
-let randomNumber = randomizeArray(questionsList);
-console.log(randomNumber);
